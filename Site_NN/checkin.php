f2cf428b9bb25d3472af4259e8338cf43d16deef
stricker.reno <stricker.reno@gmail.com>
2016-01-15 00:11:44 +0700

    fix php code

--- Site_NN/checkin.php
+++ Site_NN/checkin.php
@@ -1,5 +1,8 @@
 <?php
-   include "signup.php";
+    include "signup.php";
+    $link = mysql_connect('localhost', 'root', '') or die ('Lỗi kết nối');
+    $db=mysql_select_db('user_info',$link) or die("Failed to connect to MySQL: " . mysql_error());
+
     if (!isset($_POST['txtUsername']))
     {
         die('');
@@ -11,27 +14,33 @@
     $name       = $_POST['txtFullname'];
    
     $password=md5($password);
+
     if (!$username || !$password || !$email || !$name )
     {
         echo "Are you missing something ?!";
         exit;
     }
     
-          
-    if (mysql_num_rows(mysql_query("SELECT username FROM info_table WHERE username='$username'"))>1){
+    $di = mysql_query("SELECT * FROM info_table WHERE username='$username'");
+    if (!$di) {
+        $message  = 'Invalid query: ' . mysql_error() . "\n";
+        die($message);
+    }
+
+
+    if (mysql_num_rows($di) > 0){
         echo "This user name was taken !!";
         exit;
     }
-    if (mysql_num_rows(mysql_query("SELECT username FROM info_table WHERE username='$email'"))>1){
-        echo "This user name was taken !!";
-        exit;
-    }
+   
+  
           
-    $addmember=mysql_query("INSERT INTO `info_table`(`username`, `pass`, `name`, `email`) VALUES ('$username','$password','$name','$email')");
                           
-    if ($addmember)
+   $addmember = mysql_query("INSERT INTO `info_table`(`username`, `password`, `fullname`, `email`) VALUES('$username','$password','$name','$email')");
+
+    if($addmember)  
         echo "Register Successed !!";
     else
-        echo mysql_error();
-
+        echo mysql_error();   
+   
 ?>
