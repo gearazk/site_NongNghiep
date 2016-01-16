9c0418d7b367e70cb77d04f85f7d2d33bafd48a1
stricker.reno <stricker.reno@gmail.com>
2016-01-17 00:11:48 +0700

    add files

--- Site_NN/checkin.php
+++ Site_NN/checkin.php
@@ -21,18 +21,29 @@
         exit;
     }
     
-    $di = mysql_query("SELECT * FROM info_table WHERE username='$username'");
+    $di = mysql_query("SELECT username FROM info_table WHERE username='$username'");
     if (!$di) {
         $message  = 'Invalid query: ' . mysql_error() . "\n";
         die($message);
     }
-
-
-    if (mysql_num_rows($di) > 0){
-        echo "This user name was taken !!";
+    if (mysql_num_rows($di)>0)
+    {
+         echo "This username name was taken !! <a href='javascript: history.go(-1)'>Back</a>";
+        exit;
+    }
+    
+    if(!eregi("[_a-z0-9-]+(\.[_a-z0-9-]+)*@[_a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,4})$",$email))
+    {
+        echo "This email is not right !! <a href='javascript: history.go(-1)'>Back</a>";
+        exit;
+    }
+    if (mysql_num_rows(mysql_query("SELECT email FROM info_table WHERE email='$email'")) > 0){
+        echo "This email name was taken !! <a href='javascript: history.go(-1)'>Back</a>";
         exit;
     }
    
+    
+
   
           
                           
