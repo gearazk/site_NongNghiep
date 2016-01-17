c6bd28fac5827d6836d8c31a2edf2835925cc8f1
K10 <K10>
2016-01-17 17:44:12 +0700

    fix js check email

--- Site_NN/signup.php
+++ Site_NN/signup.php
@@ -8,7 +8,7 @@
     
     <body>
         <h1>Member Registation </h1>
-        <form action="checkin.php" method="POST">
+        <form action="checkin.php" method="POST" class="signup">
             <table >
                 <tr>
                     <td>
@@ -31,7 +31,7 @@
                         Email :
                     </td>
                     <td>
-                        <input type="text" name="txtEmail" size="20" />
+                        <input type="text" name="txtEmail" size="20" class="emailInput" />
                     </td>
                 </tr>
                 <tr>
@@ -39,31 +39,35 @@
                         Họ và tên :
                     </td>
                     <td>
-                        <input type="text" class="inputName" name="txtFullname" size="20" />
+                        <input type="text" class="inputName" name="txtFullname" size="20" maxlength="20"/>
                     </td>
                 </tr>
                 
             </table>
-            
-            <input type="submit" value="Đăng ký" />
+            <input type="button" value="Đăng ký" class="btnsubmit"/>
             
             <input type="reset" value="Nhập lại" />
             
         </form>
-        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script  type="text/javascript">
-            
-   jQuery(document).ready(function($) {
-        var max = 20;
-        $('.inputName').keypress(function(e) {
+  $(document).ready(function(){
 
-            if (this.value.length == max) {
-                e.preventDefault();
-            } else if (this.value.length > max) {
-                this.value = this.value.substring(0, max);
-            }
-        });
-    });
+$('.btnsubmit').click(function(){
+
+    validateForm();   
+});
+
+function validateForm(){
+
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    var email = $('.emailInput').val();
+    if(!emailReg.test(email)){
+       alert("email not right !")
+    }else
+        $('.signup').submit();
+   
+}});
 </script>
 
     </body>
