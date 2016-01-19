1e20ef65d2fde5248cd6db16dbb8457d20c24804
stricker.reno <stricker.reno@gmail.com>
2016-01-19 16:06:38 +0700

    new popup

--- Site_NN/user_info.sql
+++ Site_NN/user_info.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2016 at 03:51 PM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Jan 02, 2016 at 04:08 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `user_info`
@@ -26,10 +26,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `info_table`
 --
 
-CREATE TABLE `info_table` (
+CREATE TABLE IF NOT EXISTS `info_table` (
   `username` text NOT NULL,
-  `password` text NOT NULL,
-  `fullname` text NOT NULL,
+  `pass` text NOT NULL,
+  `name` text NOT NULL,
   `email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
 
@@ -37,12 +37,13 @@ CREATE TABLE `info_table` (
 -- Dumping data for table `info_table`
 --
 
-INSERT INTO `info_table` (`username`, `password`, `fullname`, `email`) VALUES
-('user001', '00123', 'nguoi dung 1', 'user001@abc.com'),
-('user02', 'pass02', 'nguoi dung 02', 'user2@abc.com'),
-('user03', 'pass03', 'nguoi dung 03', 'user3@abc.com'),
-('asfaf', '0a040ec34abbfb7f3030345244a913c9', 'sfasf', 'dsdsf'),
-('user1', '28a4565a4953cb4e7e23317ba0504f4c', 'dsfasfa', 'asfasf');
+INSERT INTO `info_table` (`username`, `pass`, `name`, `email`) VALUES
+('user001', 'pass001', 'nguoidung1', 'user1@abc.com'),
+('user002', 'pass002', 'nguoidung2', 'user2@abc.com'),
+('user003', 'pass003', 'nguoidung3', 'user3@abc.com'),
+('user004', 'pass04', 'nguoi dung 04', 'email4'),
+('user', 'pass', 'hoten', 'emial'),
+('userddd', 'asfaf', 'hoten', 'emial');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
