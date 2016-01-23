1381e806b86c12dbe048d377f444fe0dd0865d41
stricker.reno <stricker.reno@gmail.com>
2016-01-23 16:42:13 +0700

    Asf

--- checkin.php
+++ checkin.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-Type: application/json');
+
     $link = mysql_connect('localhost', 'root', '') or die ('Lỗi kết nối');
     $db=mysql_select_db('user_info',$link) or die("Failed to connect to MySQL: " . mysql_error());
 
@@ -13,7 +15,6 @@
    
     $password=md5($password);
 
-   
     
     $di = mysql_query("SELECT username FROM info_table WHERE username='$username'");
     if (!$di) {
@@ -22,21 +23,25 @@
     }
     if (mysql_num_rows($di)>0)
     {
-        echo "user";
+        $val = array('return' => 0, 'msg' => 'Tên đăng nhập này đã được sử dụng.');
+        echo json_encode($val);
         exit;
     }
     
    
     if (mysql_num_rows(mysql_query("SELECT email FROM info_table WHERE email='$email'")) > 0){
-        echo "email";
+        $val = array('return' => 0, 'msg' => 'Email này đã được sử dụng.');
+        echo json_encode($val);
         exit;
     }
                           
    $addmember = mysql_query("INSERT INTO `info_table`(`username`, `password`, `fullname`, `email`) VALUES('$username','$password','$name','$email')");
 
     if($addmember)  
-        echo "good";
+        $val = array('return' => 1, 'msg' => 'OK');
     else
-        echo mysql_error();   
-   
+        $val = array('return' => 0, 'msg' => 'Có lỗi với sever Xin quay lại sau.');
+
+//$val=$json->encode($val);
+    echo json_encode($val);
 ?>
